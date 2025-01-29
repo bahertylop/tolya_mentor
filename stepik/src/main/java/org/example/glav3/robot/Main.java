6c5a5cab363aff6a715ed272bd1039208c5d7989
bahertylop <levichankin@gmail.com>
2025-01-29 21:39:40 +0300

    отрефакторил задание с роботом

--- stepik/src/main/java/org/example/glav3/robot/Main.java
+++ stepik/src/main/java/org/example/glav3/robot/Main.java
@@ -7,49 +7,21 @@ public class Main {
     public static void moveRobot(Robot robot, int toX, int toY) {
         int x = robot.getX();
         int y = robot.getY();
-        if (toX - x > 0) {
-            if (robot.getDirection().equals(Direction.UP)) {
-                robot.turnRight();
-            } else if (robot.getDirection().equals(Direction.DOWN)) {
-                robot.turnLeft();
-            } else if (robot.getDirection().equals(Direction.LEFT)) {
-                robot.turnLeft();
-                robot.turnLeft();
-            }
-         } else {
-            if (robot.getDirection().equals(Direction.UP)) {
-                robot.turnLeft();
-            } else if (robot.getDirection().equals(Direction.DOWN)) {
-                robot.turnRight();
-            } else if (robot.getDirection().equals(Direction.RIGHT)) {
-                robot.turnLeft();
-                robot.turnLeft();
-            }
+
+        Direction directionX = (toX - x > 0) ? Direction.RIGHT : Direction.LEFT;
+        Direction directionY = (toY - y > 0) ? Direction.UP : Direction.DOWN;
+
+        while (directionX != robot.getDirection()) {
+            robot.turnLeft();
         }
-        for (int i = 0; i < Math.abs(toX - x); i++) {
+        while (robot.getX() != toX) {
             robot.stepForward();
         }
 
-        if (toY - y > 0) {
-            if (robot.getDirection().equals(Direction.RIGHT)) {
-                robot.turnLeft();
-            } else if (robot.getDirection().equals(Direction.LEFT)) {
-                robot.turnRight();
-            } else if (robot.getDirection().equals(Direction.DOWN)) {
-                robot.turnLeft();
-                robot.turnLeft();
-            }
-        } else {
-            if (robot.getDirection().equals(Direction.LEFT)) {
-                robot.turnLeft();
-            } else if (robot.getDirection().equals(Direction.RIGHT)) {
-                robot.turnRight();
-            } else if (robot.getDirection().equals(Direction.UP)) {
-                robot.turnLeft();
-                robot.turnLeft();
-            }
+        while (directionY != robot.getDirection()) {
+            robot.turnLeft();
         }
-        for (int i = 0; i < Math.abs(toY - y); i++) {
+        while (robot.getY() != toY) {
             robot.stepForward();
         }
     }
