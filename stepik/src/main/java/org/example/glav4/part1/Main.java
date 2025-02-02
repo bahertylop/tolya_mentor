1388bfa41124d8e2892454327162de53fec8c218
bahertylop <levichankin@gmail.com>
2025-02-02 15:26:15 +0300

    конпектик по исключениям

--- stepik/src/main/java/org/example/glav4/part1/Main.java
+++ stepik/src/main/java/org/example/glav4/part1/Main.java
@@ -3,11 +3,10 @@ package org.example.glav4.part1;
 public class Main {
 
     public static double sqrt(double x) {
-        if (x < 0.0) {
+        if (x < 0) {
             throw new IllegalArgumentException("Expected non-negative number, got " + x);
         }
 
         return Math.sqrt(x);
     }
-
 }
--- stepik/src/main/java/org/example/glav4/part2/RobotConnection.java
+++ stepik/src/main/java/org/example/glav4/part2/RobotConnection.java
@@ -1,5 +1,7 @@
 package org.example.glav4.part2;
 
+import java.util.zip.DataFormatException;
+
 public interface RobotConnection extends AutoCloseable {
 
     void moveRobotTo(int x, int y);
